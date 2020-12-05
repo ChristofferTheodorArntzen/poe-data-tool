19f40f02465999c446c88abc0ec727a9eabd9e57
Christoffer Arntzen <christofferarntzen@outlook.com>
2020-12-06 00:52:57 +0100

    Correct misspelling in StashTab model.

--- backend/src/main/java/com/carnnjoh/poedatatool/model/StashTab.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/model/StashTab.java
@@ -9,7 +9,7 @@ public class StashTab {
 	@JsonProperty("accountName") public String accountName;
 	@JsonProperty("lastCharacterName") public String lastCharacterName;
 	@JsonProperty("id") public String id;
-	@JsonProperty("Stash") public String stash;
+	@JsonProperty("stash") public String stash;
 	@JsonProperty("items") public List<Item> items;
 	@JsonProperty("public") public String isPublic;
 	@JsonProperty("league") public String league;
