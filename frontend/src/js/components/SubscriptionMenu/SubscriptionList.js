a84d841bd900e309ad0f408c31ea2b2cca748419
Christoffer <christofferarntzen@outlook.com>
2020-12-29 08:18:16 +0100

    Add - new components extracted from the subscriptionMenu.

---
+++ frontend/src/js/components/SubscriptionMenu/SubscriptionList.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import 
+
+class SubscriptionList extends Component {
+	constructor() {
+        super();
+	}
+
+	componentDidMount() {
+        //TODO
+    }
+
+	render() {
+
+        const 
+
+		return (
+			<div>
+				<List>
+					<ListItem></ListItem>
+				</List>
+			</div>
+		);
+	}
+}
+
+export default SubscriptionList;
