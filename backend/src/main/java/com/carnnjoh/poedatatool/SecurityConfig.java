8ece5f66f27b4b8b881241714294439bc4a6a188
Christoffer Arntzen <christofferarntzen@outlook.com>
2021-01-12 16:48:53 +0100

    Add websocket to backend. Commented out SecurityConfig for now to make the frontend able to connect and subscribe to websocket. Problem with cors filter. Add webconfig to allow same origin calls.

--- backend/src/main/java/com/carnnjoh/poedatatool/SecurityConfig.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/SecurityConfig.java
@@ -9,32 +9,46 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.Collections;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        http.httpBasic().disable();
-        http.cors().and().csrf().disable();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception
+//    {
+//        http.httpBasic().disable();
+//        http.cors().and().csrf().disable();
+//        http.authorizeRequests().antMatchers("/gs-guide-websocket/**").permitAll();
+//        http.authorizeRequests().antMatchers("/*").permitAll();
+//    }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        // config.setAllowCredentials(true); // TODO: Should add this
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(false); // TODO: Should add this
+//        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("OPTIONS");
+//        config.addAllowedMethod("HEAD");
+//        config.addAllowedMethod("GET");
+//        config.addAllowedMethod("PUT");
+//        config.addAllowedMethod("POST");
+//        config.addAllowedMethod("DELETE");
+//        config.addAllowedMethod("PATCH");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors();
+            http.csrf().disable();
+
+            http.headers().frameOptions().sameOrigin();
+        }
 }
--- backend/src/main/java/com/carnnjoh/poedatatool/schedulers/ValuableItemGenerator.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/schedulers/ValuableItemGenerator.java
@@ -6,13 +6,17 @@ import com.carnnjoh.poedatatool.model.Item;
 import com.carnnjoh.poedatatool.model.StashTab;
 import com.carnnjoh.poedatatool.model.StashTabs;
 import com.carnnjoh.poedatatool.services.PublicStashTabFetcher;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +41,9 @@ public class ValuableItemGenerator implements ScheduledTask {
 
 	private String nextChangeId;
 
+	@Autowired
+	SimpMessagingTemplate simpMessagingTemplate;
+
 	@Override
 	@Scheduled(initialDelay = 1000, fixedDelay = 10000)
 	public void execute() {
@@ -46,6 +53,7 @@ public class ValuableItemGenerator implements ScheduledTask {
 		double startTime = (double) System.currentTimeMillis();
 
 		Optional<StashTabs> metaData = fetcher.fetchPublicStashTabs(nextChangeId);
+		LOGGER.info(String.format("Http call '%.2f' secs", getDuration(startTime, (double) System.currentTimeMillis())));
 
 		List<StashTab> stashTabList = new ArrayList<>();
 
@@ -62,15 +70,17 @@ public class ValuableItemGenerator implements ScheduledTask {
 
 		itemList = itemList.stream().filter(item -> item.sockets != null).collect(Collectors.toList());
 		if (itemList.size() > 5) {
-			itemList = itemList.subList(0, 2);
+			itemList = itemList.subList(0, 1);
 		}
 
 		Random random = new Random();
 
 		for (Item item : itemList) {
 
-			ValuableItem valuableItem = new ValuableItem(item.itemId, 1, item, random.nextInt(20));
+			ValuableItem valuableItem = new ValuableItem(item.itemId, 1, item, random.nextInt(20), LocalDateTime.now());
 			valuableItemDAO.save(valuableItem);
+
+			publishToWebSockets("/topic/greetings", valuableItem);
 		}
 
 		LOGGER.info(String.format("Added %d items", itemList.size()));
@@ -85,4 +95,15 @@ public class ValuableItemGenerator implements ScheduledTask {
 		double durationMillis = endTime - startTime;
 		return durationMillis / 1000;
 	}
+
+	private void publishToWebSockets(String path, Object objectToPublish){
+		ObjectMapper objectMapper = new ObjectMapper();
+		try{
+			simpMessagingTemplate.convertAndSend(path, objectMapper.writeValueAsString(objectToPublish));
+		} catch (JsonProcessingException e) {
+			LOGGER.warn(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
 }
