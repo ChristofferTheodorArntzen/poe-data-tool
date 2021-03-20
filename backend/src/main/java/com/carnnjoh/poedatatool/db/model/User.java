8ab249575a3ce0be48d276cbee2282d2f7799710
Christoffer Arntzen <christofferarntzen@outlook.com>
2021-03-20 17:29:22 +0100

    Add new constructor, copy and toString.

--- backend/src/main/java/com/carnnjoh/poedatatool/db/model/User.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/db/model/User.java
@@ -18,4 +18,25 @@ public class User {
 	private String realm;
 	private String sessionId;
 
+	public User(String league, String accountName, String realm, String sessionId) {
+		this.league = league;
+		this.accountName = accountName;
+		this.realm = realm;
+		this.sessionId = sessionId;
+	}
+
+	public User copy() {
+		return new User(this.league, this.accountName, this.realm, this.sessionId);
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+			"pk=" + pk +
+			", league='" + league + '\'' +
+			", accountName='" + accountName + '\'' +
+			", realm='" + realm + '\'' +
+			", sessionId='" + sessionId + '\'' +
+			'}';
+	}
 }
