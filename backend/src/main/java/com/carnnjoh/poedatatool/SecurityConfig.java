714ad4191f64fd191b85fb541e07e489011d152a
Christoffer Arntzen <christofferarntzen@outlook.com>
2021-03-20 17:19:22 +0100

    Removed unnecessary imports to SecurityConfig.

--- backend/src/main/java/com/carnnjoh/poedatatool/SecurityConfig.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/SecurityConfig.java
@@ -1,17 +1,9 @@
 package com.carnnjoh.poedatatool;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import javax.inject.Singleton;
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
