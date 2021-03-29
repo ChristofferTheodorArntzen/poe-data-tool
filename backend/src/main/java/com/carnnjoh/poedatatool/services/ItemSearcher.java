8ee1efd2f95d9a818af9e5fa78b38f86c705ae28
Christoffer <christofferarntzen@outlook.com>
2021-03-30 01:06:20 +0200

    temporary service.

--- backend/src/main/java/com/carnnjoh/poedatatool/services/ItemSearcher.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/services/ItemSearcher.java
@@ -1,7 +1,14 @@
 package com.carnnjoh.poedatatool.services;
 
+import com.carnnjoh.poedatatool.model.InMemoryItem;
+import com.carnnjoh.poedatatool.model.Item;
+import com.carnnjoh.poedatatool.model.tradeAPIModels.QueryRequest.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemSearcher {
@@ -22,10 +29,42 @@ public class ItemSearcher {
 
 	private final RestTemplate template = new RestTemplate();
 
-	public void searchForItem() {
+	public List<QueryRequest> searchForItem(Map<String, InMemoryItem> inMemoryItemMap) {
+
+		List<QueryRequest> queryRequests = new ArrayList<>();
+
+		for(InMemoryItem inMemoryItem : inMemoryItemMap.values()) {
+			if(inMemoryItem.isSearch()) {
+
+				QueryRequest queryRequest = constructQueryRequest(inMemoryItem.getItem());
+				if(queryRequest != null) {
+					queryRequests.add(queryRequest);
+				}
+			}
+		}
+		return queryRequests;
+	}
+
+	//TODO: this needs to be fleshed out
+	private QueryRequest constructQueryRequest(Item item) {
 
 
 
+		List<Filter> filters = new ArrayList<>();
+		List<Stat> stats = new ArrayList<>();
+		String name = item.name;
+		String type = item.typeLine;
+
+		Status status = new Status();
+
+		Query query = new Query(status, filters ,stats, name, type);
+		Sort sort = new Sort();
+
+		if(name == null || name.isEmpty()) {
+			return null;
+		}
+
+		return new QueryRequest(query, sort);
 	}
 
 }
