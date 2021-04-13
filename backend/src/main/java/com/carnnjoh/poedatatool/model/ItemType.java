561340ab805f639152bb17cfcf5dc04e00dcad84
Christoffer Arntzen <christofferarntzen@outlook.com>
2021-04-13 22:33:48 +0200

    Add new itemtype enums, new constructors and sorted enums by "convertMods" boolean.

--- backend/src/main/java/com/carnnjoh/poedatatool/model/ItemType.java
+++ backend/src/main/java/com/carnnjoh/poedatatool/model/ItemType.java
@@ -1,35 +1,88 @@
 package com.carnnjoh.poedatatool.model;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public enum ItemType {
 
-	CORRUPTED("Corrupted", item -> item.isCorrupted),
-	ABYSSALJEWEL("Abyssal Jewels", item -> item.isAbyssalJewel),
-	SCARAB("Scarab", item -> item.typeLine.contains("Scarab")),
-	PROPHECY("Prophecy", item -> !item.prophecyText.isEmpty()),
-	FRAGMENT("Fragment", Item::isFragment),
-	OIL("Oil", item -> item.typeLine.endsWith("Oil") && item.isCurrency()),
-	CURRENCY("Currency", Item::isCurrency),
-	UNIQUE("Unique", item -> !item.name.isEmpty() && item.frameType == 3),
-	CARD("Card", item -> item.artFilename),
-	CLUSTERJEWEL("Cluster jewel", item -> item.typeLine.endsWith("Cluster Jewel")),
-	BLIGHTMAP("Blight map", item -> item.typeLine.contains("Blighted") && item.typeLine.endsWith("Map")),
-	MAP("Map", item -> item.typeLine.endsWith("Map")),
-	DELIRIUMORB("Delirium Orb", item -> item.typeLine.endsWith("Delirium Orb") && item.isCurrency()),
-	CATALYST("Catalyst", item -> item.typeLine.endsWith("Catalyst") && item.isCurrency()),
-	FOSSIL("Fossil", item -> item.typeLine.endsWith("Fossil") && item.isCurrency()),
-	ESSENCE("Essence", item -> item.typeLine.contains("Essence") && item.isCurrency()),
-	RESONATOR("Resonators", item -> item.typeLine.contains("Resonator") && item.isCurrency());
+	//CORRUPTED("Corrupted", item -> item.isCorrupted, true),
+	// static items whose mods isn't important (atleast not for now)
+	SCARAB("Scarab", item -> item.typeLine.contains("Scarab"), false),
+	PROPHECY("Prophecy", item -> item.prophecyText != null && !item.prophecyText.isEmpty(), false),
+	FRAGMENT("Fragment", Item::isFragment, false),
+	OIL("Oil", item -> item.typeLine.endsWith("Oil") && item.isCurrency(), false),
+	CURRENCY("Currency", Item::isCurrency, false),
+	UNIQUE("Unique", item -> !item.name.isEmpty() && item.frameType == 3, false),
+	CARD("Card", item -> item.artFilename, false),
+	BLIGHTMAP("Blight map", item -> item.typeLine.contains("Blighted") && item.typeLine.endsWith("Map"), false),
+	MAP("Map", item -> item.typeLine.endsWith("Map"), false),
+	DELIRIUMORB("Delirium Orb", item -> item.typeLine.endsWith("Delirium Orb") && item.isCurrency(), false),
+	CATALYST("Catalyst", item -> item.typeLine.endsWith("Catalyst") && item.isCurrency(), false),
+	FOSSIL("Fossil", item -> item.typeLine.endsWith("Fossil") && item.isCurrency(), false),
+	ESSENCE("Essence", item -> item.typeLine.contains("Essence") && item.isCurrency(), false),
+	RESONATOR("Resonators", item -> item.typeLine.contains("Resonator") && item.isCurrency(), false),
 
+	// Item types whose mods can make the item vary in price heavily
+	ABYSSALJEWEL("Abyssal Jewels", item -> item.isAbyssalJewel, true),
+	CLUSTERJEWEL("Cluster jewel", item -> item.typeLine.endsWith("Cluster Jewel"), true),
+	GEM("Gem", false),
+	UNIQUE_ARMOUR("Unique Armour", true),
+	UNIQUE_WEAPON("Unique Weapon", true),
+	UNIQUE_ACCESSORY("Unique accessory", true),
+	UNIQUE_FLASK("Unique flasks", true),
+	UNIQUE_JEWEL("Unique jewel", true),
+	UNIQUE_MAP("Unique map", true),
+	RARE_WEAPON("Rare weapon", true),
+	RARE_ARMOUR("Rare weapon", true),
+	RARE_ACCESSORIES("Rare accessories", true),
+	;
 	//TODO:	want to impl rare wep, rare armour etc. can be a pain to add tho.
 
 	public String text;
 	public Predicate<Item> filter;
+	public boolean convertMods;
 
-	ItemType(String text, Predicate<Item> filter) {
+	ItemType(String text) {
+		this(text, null, false);
+	}
+
+	ItemType(String text, boolean convertMods) {
+		this(text, null, convertMods);
+	}
+
+	ItemType(String text, Predicate<Item> filter, boolean convertMods) {
 		this.text = text;
 		this.filter = filter;
+		this.convertMods = convertMods;
+	}
+
+	public List<ItemType> getListOfItemTypes() {
+		return Arrays.asList(
+			ABYSSALJEWEL,
+			SCARAB,
+			PROPHECY,
+			FRAGMENT,
+			OIL,
+			CURRENCY,
+			UNIQUE,
+			CARD,
+			CLUSTERJEWEL,
+			BLIGHTMAP,
+			MAP,
+			DELIRIUMORB,
+			CATALYST,
+			FOSSIL,
+			ESSENCE,
+			RESONATOR,
+			GEM,
+			UNIQUE_ARMOUR,
+			UNIQUE_WEAPON,
+			UNIQUE_ACCESSORY,
+			RARE_WEAPON,
+			RARE_ARMOUR,
+			RARE_ACCESSORIES,
+			UNIQUE_FLASK);
 	}
 
 }
