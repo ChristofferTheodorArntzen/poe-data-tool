0c15bb2446b9c80914e20f201e78a43fae4f154c
Christoffer Theodor Arntzen <car@it-minds.no>
2021-04-15 18:35:12 +0200

    Add state and context for user to root of app.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,30 +1,44 @@
-import { React } from "react";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "../components/header/Header";
-import ValuableItemFeed from "../components/valuableItem/ValuableItemFeed";
-import SubscriptionMenu from "../components/subscription/SubscriptionView";
-import LoginView from "../components/login/LoginView";
-import "../styles/App.css";
+/* eslint-disable no-unused-vars */
+import React, { useState, useMemo } from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from '../components/header/Header';
+import ValuableItemFeed from '../components/valuableItem/ValuableItemFeed';
+import SubscriptionMenu from './subscription/SubscriptionMenu';
+import Login from '../components/login/Login';
+import { userContext } from '../contexts/UserContext'
+
+import '../styles/App.css';
 
 const darkTheme = createMuiTheme({
 	palette: {
-		type: "dark",
+		type: 'dark',
 	},
 });
 
+
+
 const App = () => {
+
+	const [user, setUser] = useState(null);
+
+	const userValue = useMemo(() => ({ user, setUser }), [user, setUser])
+
 	return (
-		<div className="app">
-			<Router>
-				<ThemeProvider theme={darkTheme}>
-					<Header />
-					<Route exact path="/" component={ValuableItemFeed} />
-					<Route path="/subscriptionView" component={SubscriptionMenu} />
-					<Route path="/LoginView" component={LoginView} />
-				</ThemeProvider>
-			</Router>
-		</div>
+		<React.StrictMode>
+			<userContext.Provider value={userValue}>
+				<div className='app'>
+					<Router>
+						<ThemeProvider theme={darkTheme}>
+							<Header />
+							<Route exact path='/' component={ValuableItemFeed} />
+							<Route path='/subscriptionView' component={SubscriptionMenu} />
+							<Route path='/LoginView' component={Login} />
+						</ThemeProvider>
+					</Router>
+				</div>
+			</userContext.Provider>
+		</React.StrictMode>
 	)
 }
 
