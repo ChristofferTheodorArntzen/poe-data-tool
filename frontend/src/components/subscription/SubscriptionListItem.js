77500dcf7e85629eb6cff35c4c55998d1ec984c2
Christoffer Theodor Arntzen <car@it-minds.no>
2021-04-16 19:41:42 +0200

    Continued on subscription Page. Still needs edit, delete and create functionality.

--- frontend/src/components/subscription/SubscriptionListItem.js
+++ frontend/src/components/subscription/SubscriptionListItem.js
@@ -1,73 +1,99 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { Component } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import MenuItem from "@material-ui/core/MenuItem";
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import SubscriptionModalContent from './SubscriptionModalContent';
 
-const useStyles = makeStyles((theme) => ({
+//TODO: add PropType validation
+
+const useStyles = makeStyles(() => ({
 	root: {
-		width: 400,
+		minWidth: 600,
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	title: {
+		fontSize: 14,
 	},
 	pos: {
-		marginBottom: 12,
+		marginBottom: 5,
+	},
+	active: {
+		backgroundColor: 'white',
+		color: 'black'
 	},
 }));
 
-const SubscriptionListItem = (props) => {
-	
-	console.log(props);
+const SubscriptionListItem = ({ subscription }) => {
 
-	if(props == null) {
-		return;
-	}
+	const [modalIsOpen, setModalIsOpen] = useState(false);
 
-	// const {
-	// 	id,
-	// 	name,
-	// 	tabIds,
-	// 	currencyThreshold,
-	// 	currencyType,
-	// 	itemFilter,
-	// 	isActive,
-	// } = props.subscription;
-
+	const { currencyThreshold, currencyType, isActive, itemTypes, name, pk, tabIds } = subscription;
 	const classes = useStyles();
 
+	const handleOpen = () => {
+		setModalIsOpen(true);
+	};
+
+	const handleClose = () => {
+		setModalIsOpen(false);
+	};
+
+	const body = (
+		<SubscriptionModalContent data="heu" />
+	);
+
 	return (
-		<MenuItem button dense >
-			<Card className={classes.cardStyle} variant="outlined">
-				<CardContent>
-					<Typography variant="h5" component="h5">
-						place holder sub name
-					</Typography>
-					<Typography variant="h5" component="h5">
-						{`Threshold:`}
-					</Typography>
-					<h3> Tabs: </h3>
-					<Typography >
-						tabs placeholder value
-					</Typography>
-					<h2> Item types </h2>
-					<h2> item types place holder value </h2>
-				</CardContent>
-				<div>
-					<CardActions>
-						<Button variant="contained" color="primary">
-							Edit
-								</Button>
-						<Button variant="contained" color="secondary">
-							Delete
-								</Button>
-					</CardActions>
-				</div>
-			</Card>
-		</MenuItem>
+
+		<Card key={pk} className={classes.root, (isActive ? classes.active : null)} variant='outlined'>
+			<CardContent>
+				<Typography className={classes.title} variant='h6' color='textSecondary' gutterBottom>
+					{`Name: ${name ? name : ''}`}
+				</Typography>
+				<Typography className={classes.pos} variant='subtitle1'>
+					{`Currency Threshold: ${currencyThreshold ? currencyThreshold : ''}`}
+				</Typography>
+				<Typography className={classes.pos} variant='subtitle1'>
+					{`Currency Type: ${currencyType ? currencyType : ''}`}
+				</Typography>
+				<Typography className={classes.pos} variant='subtitle1'>
+					{`Item Types: ${itemTypes ? itemTypes : ''}`}
+				</Typography>
+				<Typography className={classes.pos} variant='subtitle1'>
+					{`Tab ids: ${tabIds ? tabIds : ''} `}
+				</Typography>
+			</CardContent>
+			<CardActions >
+				<Button variant='contained' color='primary' onClick={handleOpen}>
+					Edit
+				</Button>
+				<Button variant='contained' color='secondary' onClick={handleOpen}>
+					Delete
+				</Button>
+			</CardActions>
+
+			<Modal
+				open={modalIsOpen}
+				onClose={handleClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				{body}
+			</Modal>
+
+		</Card>
+
 	)
 }
+
+
 
 export default SubscriptionListItem;
--- frontend/src/components/subscription/SubscriptionMenu.js
+++ frontend/src/components/subscription/SubscriptionMenu.js
@@ -1,19 +1,56 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react";
-import { Link } from "react-router-dom";
-import Button from "@material-ui/core/Button";
+import React, { useState, useEffect } from "react";
 import SubscriptionForm from "./SubscriptionForm";
-import SubscriptionList from "./SubscriptionList";
 
 import "../../styles/SubscriptionMenu.css";
+import { List, Typography } from "@material-ui/core";
+import { getSubscriptions } from "../../adapters/SubscriptionAdapter";
+import SubscriptionListItem from "./SubscriptionListItem";
+
+
+const constructSubscriptionRows = (subscriptions) => {
+
+	if (subscriptions == null) return [];
+
+	const rows = subscriptions.map((sub) => {
+		return (
+			<SubscriptionListItem key={sub.pk} subscription={sub} />
+		)
+	})
+
+	return rows;
+}
+
 
 const SubscriptionMenu = () => {
+
+	const [subscriptionArray, setSubscriptions] = useState(null);
+
+	useEffect(async () => {
+		try {
+			const subs = await getSubscriptions();
+			setSubscriptions(subs);
+		} catch (err) {
+			console.log(err);
+		}
+	}, []);
+
 	return (
 		<main>
+			{/* TODO: make this use 'use styles instead' */}
 			<div className="subscription-view-root">
-				<div>
-					<SubscriptionList />
+				<div className="subscription-list">
+				<Typography
+					variant="h4"
+					component="h3"
+					className="subscription-list-header"
+				>
+					List of Subscriptions:
+				</Typography>
+					<List overflow='scroll'>
+						{constructSubscriptionRows(subscriptionArray)}
+					</List>
 				</div>
 			</div>
 		</main>
