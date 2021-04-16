5cd47c1a315481bdb6132b373e23bcd315f79d22
Christoffer Theodor Arntzen <car@it-minds.no>
2021-04-16 19:39:45 +0200

    Added async

--- frontend/src/adapters/SubscriptionAdapter.js
+++ frontend/src/adapters/SubscriptionAdapter.js
@@ -3,22 +3,27 @@ import { deleteRequest, get } from './BaseAdapter'
 const devUrl = 'http://localhost:8080/subscription/';
 
 export async function getSubscriptions() {
-
     try {
-        const response = get(devUrl);
+        const response = await get(devUrl);
+        console.log(response);
 
-        return response;
+        if(response.status === 200) return response.data;
+        return null;
     } catch(err) {
         console.log(err);
     }
 
+    return null;
 }
 
 export async function deleteSubscription(data) {
-    return deleteRequest(devUrl, data)
-    .then( resp => {
-        console.log(resp);
-    })
-    .catch(err => console.log(err))
-
+    
+    try {
+        const response = await deleteRequest(devUrl, data);
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
+    
+    return null;
 }
